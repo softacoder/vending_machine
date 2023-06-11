2d3f8a034ffeb6798ceb141a836902e96fac103b
softacoder <113185323+softacoder@users.noreply.github.com>
2023-06-11 11:04:31 +0100

    Update

---
+++ script.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+
+Pragma solidity ^0.8.11;
+
+contract VendingMachine {
+    address public owner;
+    mapping (address => unit) public donutBalances;
+
+constructor() {
+    owner = msg.sender;
+    donutBalances[address(this)] = 100;
+}
+
+function getVendingMachineBalance() public view returns (uint){
+return donutBalances[address(this)];
+} 
+
+function restock(uint amount) public{
+    require(msg.sender == owner, "Only the owner can restock this machine.");
+    donutBalances[address(this)] = += amount;
+}
+
+function purchase(uint amount) public payable {
+    require(msg.value >= amount * 0.2 ether, "You must pay at least 0.2 ether per donut");
+    require(donutBalances[address(this)] => amount, "Not enough donuts in stock to fulfill purchase request");
+    donutBalances[address(this)] -= amount;
+    donutBalances[msg.sender] += amount;
+
+}
+
+}
